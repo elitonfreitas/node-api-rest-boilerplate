a8c85d1185f65a9f6d5434df3fe55dffe91336b6
Eliton Freitas <elitonfreitas@gmail.com>
2020-02-17 13:59:58 -0300

    feat: Template validation config

--- src/commons/Controller.js
+++ src/commons/Controller.js
@@ -3,11 +3,13 @@
 const RequestPromise = require('request-promise');
 const Validator = require('./utils/Validator');
 const Messages = require('./constants/Messages');
+const DataUtils = require('./utils/DataUtils');
 const Base = require('./Base');
 
 class Controller extends Base {
   constructor(request = RequestPromise) {
     super();
+    this.DataUtils = DataUtils;
     this.Validator = Validator;
     this.Messages = Messages;
     this.request = request;
